a46254f9148c12d2ebbddba08ffc252b888e3c0c
Johnson Phan <175180662+JPthecoder@users.noreply.github.com>
2025-02-21 22:27:34 -0800

    i greater than 100 which will never reach - rewrite code

--- practice.js
+++ practice.js
@@ -1,5 +1,5 @@
 // write a loop with a prompt
-for (i=0;i<100>;i++){
+/* for (i=0;i<100>;i++){
   let input = prompt("Guess a big number", '');
   if (i<100){
     input;
@@ -11,19 +11,22 @@ for (i=0;i<100>;i++){
   } 
   }
 
-console.log(i);
+console.log(i); */
 // write the prompt for a number great than a 100
 // if < 100, ask the input again
 // the loop continues until visitor enters >100
 // the loop continues until visitor cancels or enters an empty line
-/*
+
 let i = 0;
 
 while (i<100){
   let input = prompt("Guess a big number", '');
   if (i >= 100){
     alert (i);
-  } else if {
-  (!value) break;
- }
-} */
+  } else if (!input){ 
+    break;
+ } else if (i >= 100)
+  alert (i);
+} 
+
+console.log(i);
