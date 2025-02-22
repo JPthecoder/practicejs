b3f6a930e07b3d194d982133308f8a5f6f061a7e
Johnson Phan <175180662+JPthecoder@users.noreply.github.com>
2025-02-21 22:15:50 -0800

    Did a while loop which looks better

--- practice.js
+++ practice.js
@@ -13,3 +13,14 @@ console.log(i);
 // if < 100, ask the input again
 // the loop continues until visitor enters >100
 // the loop continues until visitor cancels or enters an empty line
+
+let i = 0;
+
+while (i<100){
+  let input = prompt("Guess a big number", '');
+  if (i >= 100){
+    alert (i);
+  } else if {
+  (!value) break;
+ }
+}
