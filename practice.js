aa7e157cfaa57fdb0e63adbbf6f4bfd4198ac498
Johnson Phan <175180662+JPthecoder@users.noreply.github.com>
2025-02-21 22:21:20 -0800

    Separated ! input from if..else to trigger code

--- practice.js
+++ practice.js
@@ -1,19 +1,22 @@
 // write a loop with a prompt
-for (i=0;i<500;i++){
+for (i=0;i<100>;i++){
   let input = prompt("Guess a big number", '');
   if (i<100){
     input;
-    if (!value) break;
   } else if (i > 100){
     alert(i);
+   }
+   if (!input){ 
+    break;
+  } 
   }
-}
+
 console.log(i);
 // write the prompt for a number great than a 100
 // if < 100, ask the input again
 // the loop continues until visitor enters >100
 // the loop continues until visitor cancels or enters an empty line
-
+/*
 let i = 0;
 
 while (i<100){
@@ -23,4 +26,4 @@ while (i<100){
   } else if {
   (!value) break;
  }
-}
+} */
