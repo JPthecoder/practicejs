36b38d29b3d05ef9a58d39cd09adc957f6c17e6d
Johnson Phan <175180662+JPthecoder@users.noreply.github.com>
2025-02-22 17:34:05 -0800

    Looping 3x string together without space

--- practice.js
+++ practice.js
@@ -1,18 +1,24 @@
-// create a loop to output prime numbers
-// return integer great than 1 that can't be devided without a remainder or anything execpt for 1
+const repeatString = function() {
+  // arguments string and num
+  let string = 'hey';
+  let num = 0;
 
-
-// n > 1 is a prime
-
-// write a code which output prime number in interval for 2 to n
-// n should not be hard-tuned to any fixed value
-
-let n = 10;
-
-loopy:
-  for (i = 2; i <= n; i++){
-    for (let j = 2; j < i; j++){
-      if (i % 2 == 0) continue loopy;
-    }
-      alert (i);
+  for (i = 0; i <3; i++){
+  if (num++){
+    return string += `${string}`;
   }
+
+  /* if (num == 1){
+      return `${string}`;
+}  else if ( num == 2){
+      return `${string}` + `${string}`;
+}  else {
+      return `${string}` + `${string}` + `${string}`;
+} 
+      
+return string += `${string}`; // testing this to see how I can add more than 2 string together
+*/
+    }
+};
+
+console.log(repeatString());
