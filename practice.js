510e70df7585b13f127aa8a0babbd055c12b484a
Johnson Phan <175180662+JPthecoder@users.noreply.github.com>
2025-02-27 20:42:55 -0800

    Odin exercise 02 completed

--- practice.js
+++ practice.js
@@ -1,24 +1,9 @@
-const repeatString = function (string,num){
-   
-  let returnStr = "";
-  while (num < 1){
-      returnStr += string;
-      num--;
-  }
-      return returnStr;
-  
-
-  /* if (num == 1){
-      return `${string}`;
-}  else if ( num == 2){
-      return `${string}` + `${string}`;
-}  else {
-      return `${string}` + `${string}` + `${string}`;
-} 
-      
-return string += `${string}`; // testing this to see how I can add more than 2 string together
-*/
+const repeatString = function (string, num){
+  if (num < 0) return "Error";
+    let reString = "";
+    for (i = 0; i < num; i++){
+      reString += string;
     }
-
-
-console.log(repeatString("hi", 1));
+  return reString;
+};
+console.log(repeatString("Nancy said FART", 4));
